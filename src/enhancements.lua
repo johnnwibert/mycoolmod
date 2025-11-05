a08ddfa519612b68afab23ebddb1c8f2469c44b5
John Noel Wibert <johnnoelwibert@gmail.com>
2025-11-05 00:18:47 -0500

    Geel Patch, Transmutation Joker adjustment

---
+++ src/enhancements.lua
@@ -0,0 +1,29 @@
+SMODS.Atlas {
+    key = "mycoolmod_enhancement",
+    path = "mycoolmod_enhancement.png",
+    px = 71,
+    py = 95
+}
+
+SMODS.Enhancement {
+    key = "geel",
+    pos = { x = 0, y = 0 },
+    config = {
+        h_x_mult = 1.5,
+        h_dollars = 3,
+    },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { self.config.h_x_mult, self.config.h_dollars } }
+    end,
+    loc_txt = {
+        name = "Geel Card",
+        text = {
+            "{C:white,X:red}X#1#{} Mult when held in hand",
+            "{C:money}$#2#{} if this card is",
+            "held in hand at",
+            "end of round"
+        },
+    },
+}
+
+--you can just use the enhancement key instead of e_polychrome there if you made a new enhancement 
